8581ad678e7e31e1bdbd167be42ca9190d88c055
MeganJS <247megamania@gmail.com>
2024-11-15 12:32:25 -0700

    finished jsx for play

--- src/play/simonButton.jsx
+++ src/play/simonButton.jsx
@@ -4,7 +4,9 @@ import {delay} from './delay';
 export const SimonButton = React.forwardRef(({position, onPressed}, ref) =>{
     const [lightOn, setLightOn] = React.useState(true);
     const sound = new Audio(`/${position}.mp3`);
-
+      // Use "React Refs" to allow the parent to reach into the button component
+    // and simulate a button press. This is necessary to play the sequence that
+    // the player must copy.
     React.useImperativeHandle(ref, ()=> ({
         async press(delayMs = 500, playSound = true) {
             setLightOn(false);
