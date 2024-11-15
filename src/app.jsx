638979d38f3164f9284586c145676cf4195f1a0b
MeganJS <247megamania@gmail.com>
2024-11-15 12:50:43 -0700

    set up react components for login, scores

--- src/app.jsx
+++ src/app.jsx
@@ -9,7 +9,11 @@ import { Play } from './play/play';
 import { Scores } from './scores/scores';
 import { About } from './about/about';
 
-export default function App() {
+function App() {
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);
+    
     return (
         <BrowserRouter>
             <div className='body bg-dark text-light'>
@@ -19,34 +23,45 @@ export default function App() {
                     Simon<sup>&reg;</sup>
                     </div>
                     <menu className='navbar-nav'>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to=''>
-                            Login
-                        </NavLink>
-                    </li>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to='play'>
-                            Play
-                        </NavLink>
-                    </li>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to='scores'>
-                            Scores
-                        </NavLink>
-                    </li>
-                    <li className='nav-item'>
-                        <NavLink className='nav-link' to='about'>
-                            About
-                        </NavLink>
-                    </li>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to=''>
+                                Login
+                            </NavLink>
+                        </li>
+                        <li className='nav-item'>
+                            <NavLink className='nav-link' to='about'>
+                                About
+                            </NavLink>
+                        </li>
+                        {authState === AuthState.Authenticated && (
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to='scores'>
+                                    Scores
+                                </NavLink>
+                            </li>
+                        )}
+                        {authState === AuthState.Authenticated && (
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' to='play'>
+                                    Play
+                                </NavLink>
+                            </li>
+                        )}
                     </menu>
                 </nav>
                 </header>
 
                 <main>
                 <Routes>
-                    <Route path='/' element={<Login />} exact />
-                    <Route path='/play' element={<Play />} />
+                    <Route path='/' element={<Login 
+                        userName={userName}
+                        authState={authState}
+                        onAuthChange={(userName, authState)=>{
+                            setAuthState(authState);
+                            setUserName(userName);
+                        }}
+                    />} exact />
+                    <Route path='/play' element={<Play userName="frogs"/>} />
                     <Route path='/scores' element={<Scores />} />
                     <Route path='/about' element={<About />} />
                     <Route path='*' element={<NotFound />} />
@@ -69,3 +84,5 @@ export default function App() {
 function NotFound() {
     return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
+
+export default App;
