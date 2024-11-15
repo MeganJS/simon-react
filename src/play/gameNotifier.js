06f016771f211e91e5c2ccd31c1ee3a5dd55b8fe
MeganJS <247megamania@gmail.com>
2024-11-15 12:56:55 -0700

    got it all working (by copying the github code)

--- src/play/gameNotifier.js
+++ src/play/gameNotifier.js
@@ -3,49 +3,52 @@ const GameEvent = {
     End: 'gameEnd',
     Start: 'gameStart',
   };
-
-class EventMessage {
+  
+  class EventMessage {
     constructor(from, type, value) {
       this.from = from;
       this.type = type;
       this.value = value;
     }
-}
-class GameEventNotifier {
+  }
+  
+  class GameEventNotifier {
     events = [];
     handlers = [];
-    
+  
     constructor() {
-        // Simulate chat messages that will eventually come over WebSocket
-        setInterval(() => {
-            const score = Math.floor(Math.random() * 3000);
-            const date = new Date().toLocaleDateString();
-            const userName = 'Eich';
-            this.broadcastEvent(userName, GameEvent.End, { name: userName, score: score, date: date });
-          }, 5000);
+      // Simulate chat messages that will eventually come over WebSocket
+      setInterval(() => {
+        const score = Math.floor(Math.random() * 3000);
+        const date = new Date().toLocaleDateString();
+        const userName = 'Eich';
+        this.broadcastEvent(userName, GameEvent.End, { name: userName, score: score, date: date });
+      }, 5000);
     }
-    broadcastEvent(from, type, value){
-        const event = new EventMessage(from, type, value);
-        this.receiveEvent(event);
+  
+    broadcastEvent(from, type, value) {
+      const event = new EventMessage(from, type, value);
+      this.receiveEvent(event);
     }
-
+  
     addHandler(handler) {
-        this.handlers.push(handler);
+      this.handlers.push(handler);
     }
-
-    removeHandler(handler){
-        this.handlers.filter((h)=> h !==handler);
+  
+    removeHandler(handler) {
+      this.handlers.filter((h) => h !== handler);
     }
-
+  
     receiveEvent(event) {
-        this.events.push(event);
-        this.events.forEach((e)=>{
-            this.handlers.forEach((handler) => {
-                handler(e);
-            });
+      this.events.push(event);
+  
+      this.events.forEach((e) => {
+        this.handlers.forEach((handler) => {
+          handler(e);
         });
+      });
     }
-}
-
-const GameNotifier = new GameEventNotifier();
-export { GameEvent, GameNotifier }; 
+  }
+  
+  const GameNotifier = new GameEventNotifier();
+  export { GameEvent, GameNotifier };
